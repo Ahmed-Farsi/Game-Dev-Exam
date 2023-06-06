e02faa6673744da2b496ab6cc180e12b8907cf43
Andy <Andyhoang29@gmail.com>
2023-06-06 16:16:49 +0200

    new scripts added

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,49 +1,27 @@
+// 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class PlayerController : MonoBehaviour
 {
-    public float verticalInput;
-    public float horizontalInput;
-    public float rotationSpeed = 20;
+    [SerializeField] private Rigidbody2D frontTire;
+    [SerializeField] private Rigidbody2D backTire;
+    [SerializeField] private Rigidbody2D carRigidbody;
+    [SerializeField] private float speed = 150f;
+    [SerializeField] private float rotationSpeed = 450f;
 
-    public Rigidbody2D carRigidbody;
-    public Rigidbody2D backTire;
-    public Rigidbody2D frontTire;
-    public float carTorque = 10;
-    public float speed = 5;
     private float movement;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
-        }
-    
-        horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontalInput, 0, 0);
-        transform.Translate(movement * speed * Time.deltaTime);
+        movement = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        backTire.AddTorque(movement * speed * Time.fixedDeltaTime);
-        frontTire.AddTorque(movement * speed * Time.fixedDeltaTime);
-        carRigidbody.AddTorque(movement * carTorque * Time.fixedDeltaTime);
+        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        carRigidbody.AddTorque(movement * rotationSpeed * Time.fixedDeltaTime);
     }
 }
